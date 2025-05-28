925657efead871f45b4d80d6242f23a453c8ce06
Jan Leirvaag <lja022025@gmail.com>
2025-05-28 14:13:04 +0200

    update

--- MainForm.cs
+++ MainForm.cs
@@ -16,24 +16,19 @@ public partial class Form1 : Form
     public void Initialize()
     {
         tbFirstName = new TextBox();
-        tbFirstName.Location = new System.Drawing.Point(10, 170);
-
         tbLastName = new TextBox();
-        tbLastName.Location = new System.Drawing.Point(120, 170);
-
         tbEmail = new TextBox();
-        tbEmail.Location = new System.Drawing.Point(230, 170);
 
         btnInsert = new Button();
-        btnInsert.Location = new System.Drawing.Point(10, 200);
         btnInsert.Text = "Insert";
+        btnInsert.AutoSize = true;
 
         //Hook up event
         btnInsert.Click += new EventHandler(this.btnInsert_Click);
 
         btnSelect = new Button();
-        btnSelect.Location = new System.Drawing.Point(10, 100);
         btnSelect.Text = "Select";
+        btnSelect.AutoSize = true;
 
         //Hook up event
         btnSelect.Click += new EventHandler(this.btnSelect_Click);
@@ -41,13 +36,26 @@ public partial class Form1 : Form
         rtbDataWindow = new RichTextBox();
         rtbDataWindow.Dock = DockStyle.Top;
         rtbDataWindow.ReadOnly = true;
+        rtbDataWindow.Height = 300;
+        //rtbDataWindow.Width = 400;
 
-        this.Controls.Add(btnSelect);
-        this.Controls.Add(btnInsert);
-        this.Controls.Add(rtbDataWindow);
-        this.Controls.Add(tbFirstName);
-        this.Controls.Add(tbLastName);
-        this.Controls.Add(tbEmail);
+        TableLayoutPanel table = new TableLayoutPanel();
+        table.RowCount = 100;
+        table.ColumnCount = 3;
+        table.Dock = DockStyle.Fill;
+        table.AutoSize = true;
+
+        // Add controls to specific cells
+        table.Controls.Add(rtbDataWindow, 0, 0);
+        table.SetColumnSpan(rtbDataWindow, 3);
+        table.Controls.Add(tbFirstName, 0, 10);
+        table.Controls.Add(tbLastName, 1, 10);
+        table.Controls.Add(tbEmail, 2, 10);
+        table.Controls.Add(btnSelect, 0, 20);
+        table.Controls.Add(btnInsert, 1, 20);
+
+        // Add to form
+        this.Controls.Add(table);
     }
 
 
@@ -68,13 +76,16 @@ public partial class Form1 : Form
 
         if (data.Contains(""))
         {
-
             MessageBox.Show("Fields cannot be empty!", "Warning");
             return;
         }
 
         //Send TextBox values as parameters to SQLInsert method
         Program.SQLInsert(data);
+
+        tbFirstName.Text = "";
+        tbLastName.Text = "";
+        tbEmail.Text = "";
 
     }
 
@@ -83,7 +94,7 @@ public partial class Form1 : Form
         InitializeComponent();
         this.Name = "MainForm";
         this.Text = "Main";
-        this.Size = new System.Drawing.Size(500, 500);
+        this.Size = new System.Drawing.Size(900, 500);
         this.StartPosition = FormStartPosition.CenterScreen;
 
         Initialize();
